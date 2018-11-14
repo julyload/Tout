38ad1ba8677eff7b28628e26fc7427b7b9e51b3c
July <thindotted@gmail.com>
2018-11-15 00:54:38 +0300

    Try to add new buttons instead List View items

--- app/src/main/java/com/tout/tout/MainActivity.java
+++ app/src/main/java/com/tout/tout/MainActivity.java
@@ -6,11 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.widget.Toast;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.tout.tout.entities.subjects.DataUtil;
 import com.tout.tout.entities.subjects.Subject;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,10 +21,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setRecyclerView();
-
-        initToolbar();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //initToolbar();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+/*
     private void initToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setOnMenuItemClickListener(item -> {
@@ -41,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
         });
         toolbar.inflateMenu(R.menu.menu);
     }
+*/
 
     private void setRecyclerView() {
         RecyclerView list = findViewById(R.id.subject_recycler);
--- app/src/main/java/com/tout/tout/SettingsActivity.java
+++ app/src/main/java/com/tout/tout/SettingsActivity.java
@@ -1,11 +1,15 @@
 package com.tout.tout;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.SeekBar;
 import android.widget.Button;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
 public class SettingsActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
@@ -18,10 +22,19 @@ public class SettingsActivity extends AppCompatActivity implements SeekBar.OnSee
         frequency = findViewById(R.id.frequency);
         final SeekBar seekBar = findViewById(R.id.seekBar);
         seekBar.setOnSeekBarChangeListener(this);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_action_back));
+        toolbar.setNavigationOnClickListener(v -> {
+                Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                startActivity(intent);
+        });
 
         Button changeLang = findViewById(R.id.changeLang);
         changeLang.setOnClickListener(v -> {
-            final CharSequence[] items = {"Русский", "English"};//имена методов Ваших в списке
+            final CharSequence[] items = {"Русский", "English"}; //имена методов Ваших в списке
             AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
             builder.setItems(items, (dialog, item) -> {
                 if (item == 0) {
